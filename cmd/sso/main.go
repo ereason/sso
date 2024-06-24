fc9d779283ce630be066f22c2425902eb1e4b95e
George Epifantsev <georgiy.epifantsev@eltex.loc>
2024-06-24 18:36:13 +0700

    logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	
-	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+	log.Info("starting aplication", slog.String("env", cfg.Env))
 	// TODO: инициализировать логер
-	
+
 	// TODO: инициализировать приложение (app)
-	
-	// TODO: запустить gRPC-сервер приложения 
+
+	// TODO: запустить gRPC-сервер приложения
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
